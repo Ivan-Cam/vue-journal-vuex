7ec391f2194f6ee647ecb00192066c7e99337d1d
Ivan-Campos <ivan820plus@gmail.com>
2022-04-19 15:17:04 -0500

    created base code

--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -1,4 +1,23 @@
 export default () => ({
     isLoading: true,
-    entries: [1,2,3,4,5]
+    entries: [
+        {
+            id: new Date().getTime(),
+            date: new Date().toDateString(),
+            text: 'Pariatur qui exercitation minim nulla nisi minim voluptate eiusmod nulla adipisicing.',
+            picture: null,
+        },
+        {
+            id: new Date().getTime() + 1000,
+            date: new Date().toDateString(),
+            text: 'Ut adipisicing commodo veniam non ut anim.Ut adipisicing commodo veniam non ut anim.',
+            picture: null,
+        },
+        {
+            id: new Date().getTime() + 2000,
+            date: new Date().toDateString(),
+            text: 'Laboris excepteur aute consectetur quis culpa aliquip laborum occaecat.',
+            picture: null,
+        },
+    ]
 })

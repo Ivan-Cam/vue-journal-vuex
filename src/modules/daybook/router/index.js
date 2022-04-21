8d094452ce9e12f87c657cb58788812e5387fcd6
Ivan-Campos <ivan820plus@gmail.com>
2022-04-21 09:57:45 -0500

    finished basic journal app

--- src/modules/daybook/router/index.js
+++ src/modules/daybook/router/index.js
@@ -7,11 +7,17 @@ export default {
             path: '',
             name: 'no-entry',
             component: () => import(/* webpackChunkName: "daybook-no-entry"*/ '@/modules/daybook/views/NoEntrySelected.vue'),   
+
         },
         {
             path: ':id',
             name: 'entry',
             component: () => import(/* webpackChunkName: "daybook-entry"*/ '@/modules/daybook/views/EntryView.vue'), 
+            props: ( route ) => {
+                return {
+                    id: route.params.id
+                }
+            }
         }
     ]
 
--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -1,23 +1,23 @@
 export default () => ({
-    isLoading: true,
-    entries: [
-        {
-            id: new Date().getTime(),
-            date: new Date().toDateString(),
-            text: 'Pariatur qui exercitation minim nulla nisi minim voluptate eiusmod nulla adipisicing.',
-            picture: null,
-        },
-        {
-            id: new Date().getTime() + 1000,
-            date: new Date().toDateString(),
-            text: 'Ut adipisicing commodo veniam non ut anim.Ut adipisicing commodo veniam non ut anim.',
-            picture: null,
-        },
-        {
-            id: new Date().getTime() + 2000,
-            date: new Date().toDateString(),
-            text: 'Laboris excepteur aute consectetur quis culpa aliquip laborum occaecat.',
-            picture: null,
-        },
-    ]
-})
+  isLoading: true,
+  entries: [
+    {
+      id: '1',
+      date: new Date().toDateString(),
+      text: "Pariatur qui exercitation minim nulla nisi minim voluptate eiusmod nulla adipisicing.Pariatur qui exercitation minim nulla nisi minim voluptate eiusmod nulla adipisicing.Pariatur qui exercitation minim nulla nisi minim voluptate eiusmod nulla adipisicing.Pariatur qui exercitation minim nulla nisi minim voluptate eiusmod nulla adipisicing.",
+      picture: null,
+    },
+    {
+      id: '2',
+      date: new Date().toDateString(),
+      text: "Ut adipisicing commodo veniam non ut anim.Ut adipisicing commodo veniam non ut anim.",
+      picture: null,
+    },
+    {
+      id: '3',
+      date: new Date().toDateString(),
+      text: "Laboris excepteur aute consectetur quis culpa aliquip laborum occaecat.",
+      picture: null,
+    },
+  ],
+});
